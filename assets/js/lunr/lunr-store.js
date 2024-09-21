b4299769d601b5294cf859a1a3f62a2fb7afb614
grimzaeye <grimzaeye@users.noreply.github.com>
2024-09-21 14:47:48 +0000

    deploy: 508e9fb13545ce762a40265ef066a754183ff95c

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,103 @@
+var store = [{
+        "title": "Peanut Butter Cookies",
+        "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
+        "tags": [],
+        "url": "/news/peanut-butter-cookies/",
+        "teaser": "/assets/images/fallback500x300.png"
+      },{
+        "title": "Chocolate Chip Cookies",
+        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
+        "tags": [],
+        "url": "/news/chocolate-chip-cookies/",
+        "teaser": "/assets/images/fallback500x300.png"
+      },{
+        "title": "Oatmeal Cookies",
+        "excerpt":"Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts. Ingredients 1 cup butter, softened 1 cup white sugar 1 cup packed brown sugar 2 eggs 1 teaspoon vanilla extract 2 cups all-purpose flour 1...","categories": [],
+        "tags": [],
+        "url": "/news/oatmeal-cookies/",
+        "teaser": "/assets/images/fallback500x300.png"
+      },{
+        "title": "Edge Case: Many Categories",
+        "excerpt":"This post has many categories.  ","categories": ["Research"],
+        "tags": ["categories","edge case"],
+        "url": "/research/edge-case-many-categories/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Edge Case: No Body Content",
+        "excerpt":" ","categories": ["research"],
+        "tags": ["research"],
+        "url": "/research/edge-case-no-body-content/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Edge Case No Yaml Title",
+        "excerpt":"This post has no title specified in the YAML Front Matter. Jekyll should auto-generate a title from the filename.  ","categories": ["research"],
+        "tags": ["research"],
+        "url": "/research/edge-case-no-yaml-title/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Post: Gallery",
+        "excerpt":"These are gallery tests for image wrapped in &lt;figure&gt; elements. To place a gallery add the necessary YAML Front Matter: gallery: - url: /assets/images/unsplash-gallery-image-1.jpg image_path: /assets/images/unsplash-gallery-image-1-th.jpg alt: \"placeholder image 1\" title: \"Image 1 title caption\" - url: /assets/images/unsplash-gallery-image-2.jpg image_path: /assets/images/unsplash-gallery-image-2-th.jpg alt: \"placeholder image 2\" title: \"Image 2 title caption\" -...","categories": ["arts"],
+        "tags": ["gallery","Post Formats","tiled"],
+        "url": "/arts/post-gallery/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Layout: Header Image and Text Readability",
+        "excerpt":"This is a sample post with a large feature image1 up top and tons of text. Odio ad blue bottle vinyl, 90’s narwhal commodo bitters pour-over nostrud. Ugh est hashtag in, fingerstache adipisicing laboris esse Pinterest shabby chic Portland. Shoreditch bicycle rights anim, flexitarian laboris put a bird on it...","categories": ["arts"],
+        "tags": ["sample post","readability","test"],
+        "url": "/arts/layout-header-image-text-readability/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Markup: HTML Tags and Formatting",
+        "excerpt":"A variety of common markup showing how the theme styles them. Header two Header three Header four Header five Header six Blockquotes Single line blockquote: Stay hungry. Stay foolish. Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to focus on....","categories": ["arts"],
+        "tags": ["content","css","formatting","html","markup"],
+        "url": "/arts/markup-html-tags-and-formatting/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Markup: Syntax Highlighting",
+        "excerpt":"Syntax highlighting is a feature that displays source code, in different colors and fonts according to the category of terms. This feature facilitates writing in a structured language such as a programming language or a markup language as both structures and syntax errors are visually distinct. Highlighting does not affect...","categories": ["arts"],
+        "tags": ["code","syntax highlighting"],
+        "url": "/arts/markup-syntax-highlighting/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "arts-Ginger Gulp Identity-arts",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": ["arts"],
+        "tags": [],
+        "url": "/projects/2009-08-06-ginger-gulp-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "art-Fizz Bang Identity-p",
+        "excerpt":"arts Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War...","categories": ["arts"],
+        "tags": [],
+        "url": "/projects/art-fizz-bang-identity-p/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "arts-Fizz Bang Identity-debug",
+        "excerpt":"페이지 카테고리 arts Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening...","categories": ["arts"],
+        "tags": ["stupid"],
+        "url": "/projects/arts-fizz-bang-identity-debug/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "research - Foo Bar Identity-arts categories",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": ["research"],
+        "tags": ["article"],
+        "url": "/projects/foo-bar-website/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "research - Baz Boom Identity-p",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": ["research"],
+        "tags": [],
+        "url": "/projects/research-baz-boom-identity-p/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "research - Baz Boom Identity-debug",
+        "excerpt":"research - Baz Boom Identity-debug Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn&#8217;t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered...","categories": ["research"],
+        "tags": [],
+        "url": "/projects/research-baz-boom-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "width test",
+        "excerpt":" ","categories": ["research"],
+        "tags": [],
+        "url": "/projects/width-test/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      }]
